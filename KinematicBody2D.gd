de08a45db268166ed1f9ef15c95b4cc0dd06cef5
Brennanzuz <timingout365@gmail.com>
2021-10-14 21:53:13 +0800

    Changed movement to sliding type
    Changed code to use action_strength

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,27 +1,36 @@
 extends KinematicBody2D
 
-export var speed = 100
+export var ACCELERATION = 500
+export var MAX_SPEED = 100
+export var FRICTION = 500
+
+var velocity = Vector2.ZERO 
+
 var is_player = true
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
+	var input_vector = Vector2.ZERO
 	# Make the player move when the arrow keys pressed
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector!=Vector2.ZERO:
+		#ACCELERATION is frame based while delta makes it pixel based.
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		#Apply friction when no input
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
+	
 	if Input.is_action_pressed("ui_left"): 
-		velocity.x -= 1
 		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("ui_right"): 
-		velocity.x += 1
 		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed("ui_up"): 
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"): 
-		velocity.y += 1
 	if velocity == Vector2.ZERO:
 		$AnimatedSprite.play("idle")
 	else:
 		$AnimatedSprite.play("run")
-	move_and_slide(velocity * speed)
-	
-
 
 func _on_Area2D_body_entered(body):
 	hide()
